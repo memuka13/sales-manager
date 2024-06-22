346e0af975457749f11066a111ec7147895474a6
Mamuka Jincharadze <mamuka_jincharadze@yahoo.com>
2024-06-22 15:20:53 +0400

    add reactiveForms to login page

--- src/app/pages/log-in/log-in.component.ts
+++ src/app/pages/log-in/log-in.component.ts
@@ -3,12 +3,31 @@ import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { TranslateModule } from '@ngx-translate/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [CommonModule, MatFormFieldModule, MatInputModule, TranslateModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    TranslateModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './log-in.component.html',
   styleUrls: ['./log-in.component.scss'],
 })
-export class LogInComponent {}
+export class LogInComponent {
+  readonly form = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+}
