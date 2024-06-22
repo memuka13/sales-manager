8ed41a485f0514e769bed909f80f06346536d7f6
Mamuka Jincharadze <mamuka_jincharadze@yahoo.com>
2024-06-22 21:34:01 +0400

    add products table and data

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,13 +1,57 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ELEMENT_DATA, Product } from './products.data';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    TranslateModule,
+  ],
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit, AfterViewInit {
+  readonly displayedColumns: string[] = ['title', 'price', 'quantity'];
+  readonly dataSource = new MatTableDataSource<Product>(ELEMENT_DATA);
 
+  @ViewChild(MatPaginator) paginator?: MatPaginator;
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  getFilterPredicate() {
+    return (row: Product, filter: string) => {
+      const matchFilter = [];
+      const columnTitle = row.title;
+      const customFilterTitle = columnTitle.toLowerCase().includes(filter);
+      matchFilter.push(customFilterTitle);
+      return matchFilter.every(Boolean);
+    };
+  }
+
+  ngOnInit() {
+    this.dataSource.filterPredicate = this.getFilterPredicate();
+  }
+
+  ngAfterViewInit() {
+    if (this.paginator) this.dataSource.paginator = this.paginator;
+  }
+  openDialog(row: Product) {
+    console.log(row);
+  }
 }
