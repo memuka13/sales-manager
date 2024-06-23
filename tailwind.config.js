4c6df8c6af0fa5c192a959fbdf7ed7949368feeb
Mamuka Jincharadze <mamuka_jincharadze@yahoo.com>
2024-06-23 18:56:28 +0400

    implement add, edit and sell product

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,26 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        ".no-arrows": {
+          // For Chrome, Safari, Edge, and Opera
+          "&::-webkit-outer-spin-button": {
+            "-webkit-appearance": "none",
+            margin: "0",
+          },
+          "&::-webkit-inner-spin-button": {
+            "-webkit-appearance": "none",
+            margin: "0",
+          },
+          // For Firefox
+          "&[type=number]": {
+            "-moz-appearance": "textfield",
+          },
+        },
+      });
+    },
+  ],
   corePlugins: { preflight: false },
 };
