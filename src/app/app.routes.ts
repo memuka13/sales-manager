b531a556c83621a87a9074b5f801a68ea900ec5b
Mamuka Jincharadze <mamuka_jincharadze@yahoo.com>
2024-06-23 14:15:30 +0400

    initiate register page functionality and validators

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,17 +12,24 @@ export const appRoutes: Route[] = [
       import('./pages/log-in/log-in.component').then((m) => m.LogInComponent),
   },
   {
+    path: 'register',
+    loadComponent: () =>
+      import('./pages/register/register.component').then(
+        (m) => m.RegisterComponent
+      ),
+  },
+  {
     path: 'products',
     loadComponent: () =>
       import('./pages/products/products.component').then(
-        (m) => m.ProductsComponent,
+        (m) => m.ProductsComponent
       ),
   },
   {
     path: 'sales-managers',
     loadComponent: () =>
       import('./pages/sales-managers/sales-managers.component').then(
-        (m) => m.SalesManagersComponent,
+        (m) => m.SalesManagersComponent
       ),
   },
   {
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { passwordMatchValidator } from './validators';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    TranslateModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
-
+  readonly form = new FormGroup(
+    {
+      username: new FormControl('', Validators.required),
+      name: new FormControl('', Validators.required),
+      surname: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      confirmPassword: new FormControl('', Validators.required),
+    },
+    { validators: passwordMatchValidator() }
+  );
+  submit() {}
 }
